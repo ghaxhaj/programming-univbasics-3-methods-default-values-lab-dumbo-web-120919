32793e790c5cae79971d70651bfa25378b7cfd39
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-11-30 22:34:47 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@ def meal_choice(veg1,veg2,protein="meat")
   puts "A plate of #{protein} with #{veg1} and #{vet2}."
 end
 
-veg1=string beans
-veg2=tomatoes
+veg1="string beans"
+veg2="tomatoes"
 
 meal_choice
